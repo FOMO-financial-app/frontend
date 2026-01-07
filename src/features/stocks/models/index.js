970311dfddad062a636083b0f0f3be2fb8f5bf66
GoroAlejandro <alejandro.goro100@gmail.com>
2026-01-07 15:33:05 -0300

    Reapply "feat: implement stock charts with indicators and options panel (#9)"
    This reverts commit 2e833b92860f8132954f1ab402362b9eadb5cb18.

---
+++ src/features/stocks/models/index.js
@@ -0,0 +1,4 @@
+export * from "./mappers/mapBands.js"
+export * from "./mappers/mapMainChannel.js"
+export * from "./mappers/mapSingleValue.js"
+export * from "./mappers/mapToCandleStick.js"
--- src/features/stocks/pages/HomePage.jsx
+++ src/features/stocks/pages/HomePage.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { stockService } from "../services/stockService";
-import { StockTable } from "../components/paginatedtable/StockTable";
-import "./HomePage.css"
-import { PaginationControls } from "../components/paginatedtable/PaginationControl";
+import { stockService } from "../services";
+import { StockTable, PaginationControls } from "../components";
 import TradingViewWidget from "../components/TradingViewWidget";
+import "./HomePage.css"
 
 export const HomePage = () => {
     const [ highlightedIndex, setHighlightedIndex ] = useState(-1);
